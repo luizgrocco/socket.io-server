e8723742e88e829fb8d81d19ea6ed962781a7d81
Luiz Rocco <luizgustavorocco@gmail.com>
2024-04-19 02:11:06 -0300

    NanoID for room no generation, also adds prettier

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import { customAlphabet } from "nanoid";
+const ID_ALPHABET =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+const ID_LENGTH = 10;
+
+// Create a NanoID generator using the custom alphabet
+const generateID = customAlphabet(ID_ALPHABET, ID_LENGTH);
 // import { fileURLToPath } from "node:url";
 // import { dirname, join } from "node:path";
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
 
 // const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -17,18 +28,18 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 
-io.on("connection", socket => {
-  console.log("connection inbound", socket.id);
+io.on("connection", (socket) => {
+  console.log(`${socket.id} connected`);
 
-  socket.on("create_room", ({ username, cubeState = {}, queueState = [] }) => {
+  socket.on("create_room", ({ username, cubeState = [], queueState = [] }) => {
+    console.log("create_room received");
     // TODO: Validate all required fields to create a room
     if (!username) {
       socket.emit("create_room_error", { message: "Invalid username" });
       return;
     }
 
-    // TODO: create better roomId (username can easily clash)
-    const roomId = `room:${username}`;
+    const roomId = generateID();
 
     if (rooms[roomId]) {
       socket.emit("create_room_error", { message: "Room already exists" });
@@ -49,6 +60,7 @@ io.on("connection", socket => {
   });
 
   socket.on("join_room", ({ roomId, username }) => {
+    console.log("join_room received");
     if (!roomId || !rooms[roomId]) {
       socket.emit("join_room_error", { message: "Room does not exist" });
       return;
@@ -79,13 +91,26 @@ io.on("connection", socket => {
   });
 
   // Require roomId, can infer from users table, but shouldn't
-  socket.on("move", (move, roomId) => {});
+  socket.on("enqueue_move", ({ roomId, move }) => {});
 
   // Since no data is passed on disconnect, only way is to infer roomId
   socket.on("disconnect", () => {
-    const disconnectedRoomId = users[socket.id].roomId;
-    rooms[disconnectedRoomId].users.filter(user => user.socketId !== socket.id);
+    console.log(`${socket.id} disconnected`);
+    const disconnectedRoomId = users?.[socket.id]?.roomId;
+    if (disconnectedRoomId) {
+      delete users[socket.id];
+      rooms[disconnectedRoomId].users.filter(
+        (user) => user.socketId !== socket.id
+      );
+    }
   });
+});
+
+io.engine.on("connection_error", (err) => {
+  console.log(err.req); // the request object
+  console.log(err.code); // the error code, for example 1
+  console.log(err.message); // the error message, for example "Session ID unknown"
+  console.log(err.context); // some additional error context
 });
 
 server.listen(3000, () => {
